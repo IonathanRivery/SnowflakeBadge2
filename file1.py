1344e58900db12af9f625916b20316954edb7b76
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-26 15:51:26 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("Hello! Rivery")
 streamlit.header("This is a header sample 1")
@@ -7,3 +8,8 @@ streamlit.text("This is a text sample 2")
 streamlit.text("This is a text sample 3")
 streamlit.text("This is a text sample 4")
 streamlit.header("This is a header sample 2")
+
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
