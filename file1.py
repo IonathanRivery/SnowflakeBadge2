121358db1019201d354f62f398b71963d16205dc
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-26 18:07:05 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -9,6 +9,7 @@ streamlit.text("This is a text sample 3")
 streamlit.text("This is a text sample 4")
 streamlit.header("This is a header sample 2")
 
+
 streamlit.header("My Fruit List")
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -16,11 +17,23 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Creating a selection for the user with streamlit.multiselect()
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+selected_fruits = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), default=list(my_fruit_list.index))
 
-# Filter the dataframe based on the selected fruits
-filtered_fruit_list = my_fruit_list.loc[selected_fruits]
+# Check if any fruit is selected
+if selected_fruits:
+    # Filter the dataframe based on the selected fruits
+    filtered_fruit_list = my_fruit_list.loc[selected_fruits]
+    # Present the filtered table
+    streamlit.dataframe(filtered_fruit_list)
+else:
+    # Present the entire table
+    streamlit.dataframe(my_fruit_list)
 
-# Present the table
-streamlit.dataframe(filtered_fruit_list)
-
+# Add a callback to update the table when the user changes the selected fruits
+def update_table():
+    selected_fruits = multiselect_widget.current_value
+    if selected_fruits:
+        filtered_fruit_list = my_fruit_list.loc[selected_fruits]
+        table_widget.dataframe(filtered_fruit_list)
+    else:
+        table_widget.dataframe(my_fruit_list)
