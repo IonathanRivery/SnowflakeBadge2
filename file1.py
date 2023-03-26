3b9e0098f0dd735b0ceaf8fc95779980de36b3c3
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-26 15:52:20 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -9,7 +9,7 @@ streamlit.text("This is a text sample 3")
 streamlit.text("This is a text sample 4")
 streamlit.header("This is a header sample 2")
 
-
+streamlit.header("My Fruit List")
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
