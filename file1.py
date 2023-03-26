508761627d5b8d0841039a3fee62a59a0600d0e9
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-26 17:32:55 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -13,11 +13,11 @@ streamlit.header("My Fruit List")
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # setting an index by the column "Fruit" - indexes will be taken from this column instead of default number indexes
-my_fruit_list = my_fruit_list.set_index("Fruit")
+my_fruit_list_indexed = my_fruit_list.set_index("Fruit")
 
 # Creating a selection for the user with streamlit.multiselect()
 streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 
 # Present the table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list_indexed)
 
