1247aec544ec1a9939a51390e6eeed7f6a227754
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-26 15:41:15 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("Hello! Rivery")
+streamlit.header("This is a header sample 1")
+streamlit.text("This is a text sample 1")
+streamlit.text("This is a text sample 2")
+streamlit.text("This is a text sample 3")
+streamlit.text("This is a text sample 4")
+streamlit.header("This is a header sample 2")
