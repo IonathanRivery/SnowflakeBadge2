f2ba60212aad6504daa3d90357e7896c0eabe673
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-29 15:46:34 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -43,6 +43,6 @@ def update_table():
 import requests    
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
 streamlit.text(fruityvice_response)
 streamlit.text(fruityvice_response.json())
