2781cbe4908ee428c7296965804121357e06652c
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-29 16:42:01 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -47,9 +47,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
 streamlit.text(fruityvice_response)
 fruit_choice = streamlit.multiselect("Pick some fruits you would like to get some more info on: ", list(my_fruit_list.index))
 streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # take the json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # output it the screen as a table
 streamlit.dataframe (fruityvice_normalized)
 
