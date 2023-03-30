ec44881cdeec3e25aa642c523637da88d878ed9b
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-30 13:48:57 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -45,16 +45,17 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
 streamlit.text(fruityvice_response)
-fruit_choice = streamlit.multiselect("Pick some fruits you would like to get some more info on: ", list(my_fruit_list.index))
+fruityvice_list = pd.read_csv("https://fruityvice.com/api/fruit/all")
+fruit_choice = streamlit.multiselect("Pick some fruits you would like to get some more info on: ", list(fruityvice_list))
 # Check if any fruit is selected
 if fruit_choice:
 # Get the selected fruit
-    selected_fruit = fruit_choice
-    streamlit.text('The user selected: ' + str(selected_fruit))
+    selected_fruityvice_fruit = fruit_choice
+    streamlit.text('The user selected: ' + str(selected_fruityvice_fruit))
 # Send the API request for the selected fruit
-    fruityvice_response1 = requests.get("https://fruityvice.com/api/fruit/" + str(selected_fruit))
+    fruityvice_fruit_select = requests.get("https://fruityvice.com/api/fruit/" + str(selected_fruityvice_fruit))
 # Take the json version of the response and normalize it
-    fruityvice_normalized = pd.json_normalize(fruityvice_response1.json())
+    fruityvice_normalized = pd.json_normalize(fruityvice_fruit_select.json())
 # Output it to the screen as a table
     streamlit.dataframe(fruityvice_normalized)
 else:
