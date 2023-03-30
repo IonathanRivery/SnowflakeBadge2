8e05a454c71cedc1d9e96f84b5cc01c171a662e8
IonathanRivery <128492454+IonathanRivery@users.noreply.github.com>
2023-03-30 11:25:51 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -62,7 +62,7 @@ fruit_choice = streamlit.multiselect("Pick some fruits you would like to get som
 # Check if any fruit is selected
 if fruit_choice:
     # Get the selected fruit
-    selected_fruit = fruit_choice[0]
+    selected_fruit = fruit_choice[]
     streamlit.text('The user selected: ' + selected_fruit)
     # Send the API request for the selected fruit
     fruityvice_response1 = requests.get("https://fruityvice.com/api/fruit/" + selected_fruit)
